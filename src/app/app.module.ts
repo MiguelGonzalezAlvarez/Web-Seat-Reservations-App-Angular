0e5f04a7683ac16798ce2c59125c9d7277f105f5
MiguelMuros <miggonzalv@gmail.com>
2024-02-12 12:38:55 +0100

    chore: configure app component and library dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReservationsModule } from './reservations/reservations.module';
+import { SharedModule } from './shared/shared.module';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    ReservationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
